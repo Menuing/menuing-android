ee07364c2d8d73830d846c2ea95e865282b63037
marc.ribalta <marc.ribalta@eurecat.org>
2018-11-01 11:19:03 +0100

    moved user profile activity

--- app/src/main/java/com/example/mrg20/menuing_android/activities/UserProfile.java
+++ app/src/main/java/com/example/mrg20/menuing_android/activities/UserProfile.java
@@ -1,7 +1,9 @@
-package com.example.mrg20.menuing_android;
+package com.example.mrg20.menuing_android.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.example.mrg20.menuing_android.R;
 
 public class UserProfile extends AppCompatActivity {
 
