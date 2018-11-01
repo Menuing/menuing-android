544d6d0e223f6ab54fb57d858c23dc90cfd7ed4c
marc.ribalta <marc.ribalta@eurecat.org>
2018-11-01 16:32:46 +0100

    added onclicklistener way

--- app/src/main/java/com/example/mrg20/menuing_android/activities/UserProfile.java
+++ app/src/main/java/com/example/mrg20/menuing_android/activities/UserProfile.java
@@ -4,30 +4,40 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.mrg20.menuing_android.R;
 
-public class UserProfile extends AppCompatActivity {
+public class UserProfile extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        Button allergies = (Button) findViewById(R.id.my_allergies);
+        Button tastes = (Button) findViewById(R.id.my_tastes);
+        Button termsAndConditions = (Button) findViewById(R.id.info_terms_conditions);
+
+        allergies.setOnClickListener(this);
+        tastes.setOnClickListener(this);
+        termsAndConditions.setOnClickListener(this);
     }
 
-    public void seeAllergies(View view) {
-        Intent intent = new Intent(UserProfile.this, AllergiesActivity.class);
+    @Override
+    public void onClick(View view) {
+        Intent intent = null;
+        switch(view.getId()) {
+            case R.id.info_terms_conditions:
+                intent = new Intent(UserProfile.this, TermsAndConditionActivity.class);
+                break;
+            case R.id.my_allergies:
+                intent = new Intent(UserProfile.this, AllergiesActivity.class);
+                break;
+            case R.id.my_tastes:
+                intent = new Intent(UserProfile.this, TastesActivity.class);
+                break;
+        }
         startActivity(intent);
     }
-
-    public void seeTastes(View view) {
-        Intent intent = new Intent(UserProfile.this, TastesActivity.class);
-        startActivity(intent);
-    }
-
-    public void seeTermsAndConditions(View view) {
-        Intent intent = new Intent(UserProfile.this, TermsAndConditionActivity.class);
-        startActivity(intent);
-    }
-
 }
