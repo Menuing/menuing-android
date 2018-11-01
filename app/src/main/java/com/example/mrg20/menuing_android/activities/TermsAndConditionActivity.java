5a4c718a4c60e04d3113c5b896eaa5bb5a8910e1
Lluis <hykrenx@gmail.com>
2018-11-01 13:42:10 +0100

    Added an activity to view the terms and conditions

---
+++ app/src/main/java/com/example/mrg20/menuing_android/activities/TermsAndConditionActivity.java
@@ -0,0 +1,28 @@
+package com.example.mrg20.menuing_android.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.mrg20.menuing_android.R;
+
+public class TermsAndConditionActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.terms_and_conditions);
+
+        Button closeButton = (Button) findViewById(R.id.termsconditions_CrossButton);
+        closeButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.termsconditions_CrossButton:
+                finish();
+        }
+    }
+}
