c2351f41b28fb9f8f5b59fe2f5e60e31973cbd4b
marc.ribalta <marc.ribalta@eurecat.org>
2018-11-01 14:00:57 +0100

    added buttons to save and delete

--- app/src/main/java/com/example/mrg20/menuing_android/activities/UserProfile.java
+++ app/src/main/java/com/example/mrg20/menuing_android/activities/UserProfile.java
@@ -1,7 +1,9 @@
 package com.example.mrg20.menuing_android.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.example.mrg20.menuing_android.R;
 
@@ -12,4 +14,20 @@ public class UserProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
     }
+
+    public void seeAllergies(View view) {
+        Intent intent = new Intent(UserProfile.this, AllergiesActivity.class);
+        startActivity(intent);
+    }
+
+    public void seeTastes(View view) {
+        Intent intent = new Intent(UserProfile.this, TastesActivity.class);
+        startActivity(intent);
+    }
+
+    public void seeTermsAndConditions(View view) {
+        // Intent intent = new Intent(UserProfile.this, .class);
+        // startActivity(intent);
+    }
+
 }
