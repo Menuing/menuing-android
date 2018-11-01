2d83415f946a56a546b4e3c4b55fc56d60c7c596
marc.ribalta <marc.ribalta@eurecat.org>
2018-11-01 19:30:43 +0100

    created recipe details activity

---
+++ app/src/main/java/com/example/mrg20/menuing_android/activities/RecipeDetails.java
@@ -0,0 +1,15 @@
+package com.example.mrg20.menuing_android.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mrg20.menuing_android.R;
+
+public class RecipeDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_details);
+    }
+}
