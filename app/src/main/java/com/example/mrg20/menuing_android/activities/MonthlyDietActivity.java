83985c55c00e279695e8bfbce86dcb06ac4064b9
Alba Lamas <alba.v.lamas@gmail.com>
2018-11-01 16:54:06 +0100

    when click in a day, view de diet of that day

--- app/src/main/java/com/example/mrg20/menuing_android/activities/MonthlyDietActivity.java
+++ app/src/main/java/com/example/mrg20/menuing_android/activities/MonthlyDietActivity.java
@@ -1,20 +1,24 @@
 package com.example.mrg20.menuing_android.activities;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.CalendarView;
 
 import com.example.mrg20.menuing_android.R;
+import com.example.mrg20.menuing_android.ViewMealActivity;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-public class MonthlyDietActivity extends AppCompatActivity {
+public class MonthlyDietActivity extends AppCompatActivity implements CalendarView.OnDateChangeListener {
 
     CalendarView calendarView;
     @Override
@@ -34,6 +38,37 @@ public class MonthlyDietActivity extends AppCompatActivity {
 
         calendarView.setMinDate(dateMin);
         calendarView.setMaxDate(dateMax);
+
+        calendarView.setOnDateChangeListener(this);
+    }
+
+    @Override
+    public void onSelectedDayChange(CalendarView view, final int year, final int month, final int dayOfMonth) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        CharSequence []items = new CharSequence[2];
+        items[0] = "Ver";
+        items[1] = "Cancelar";
+
+        builder.setTitle("Selecciona una tarea")
+                .setItems(items, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if(which==0){
+                            Intent intent = new Intent(getApplication(),ViewMealActivity.class);
+                            Bundle bundle = new Bundle();
+                            bundle.putInt("dia", dayOfMonth);
+                            bundle.putInt("mes", month);
+                            bundle.putInt("any", year);
+                            intent.putExtra("data", bundle);
+                            startActivity(intent);
+                        }else{
+                            return;
+                        }
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
 
     }
 }
