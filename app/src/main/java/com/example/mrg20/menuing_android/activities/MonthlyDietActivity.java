099df99bd4bb21c06328ce365afe999967b1d740
Alba Lamas <alba.v.lamas@gmail.com>
2018-11-04 13:05:43 +0100

    add return button in monthly diet activity

--- app/src/main/java/com/example/mrg20/menuing_android/activities/MonthlyDietActivity.java
+++ app/src/main/java/com/example/mrg20/menuing_android/activities/MonthlyDietActivity.java
@@ -3,6 +3,7 @@ package com.example.mrg20.menuing_android.activities;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.CalendarView;
@@ -19,6 +20,10 @@ public class MonthlyDietActivity extends AppCompatActivity implements CalendarVi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monthly_diet);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
         CalendarView calendarView = findViewById(R.id.monthlyView);
 
@@ -63,6 +68,11 @@ public class MonthlyDietActivity extends AppCompatActivity implements CalendarVi
 
         AlertDialog dialog = builder.create();
         dialog.show();
+    }
 
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
     }
 }
