c2bad7bab46aefbb892dd6cd4b8e18c0dea66975
Adria331 <adria331133@gmail.com>
2018-12-22 18:32:18 +0100

    fix methods helper

--- app/src/main/java/com/example/mrg20/menuing_android/DatabaseHelper.java
+++ app/src/main/java/com/example/mrg20/menuing_android/DatabaseHelper.java
@@ -60,7 +60,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
         long result = db.insert(TABLE_NAME, null, contentValues);
 
-        //if date as inserted incorrectly it will return -1
         if (result == -1) {
             return false;
         } else {
@@ -80,46 +79,16 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
 
     /**
-     * Returns only the ID that matches the name passed in
-     * @param name
-     * @return
-     */
-    public Cursor getItemID(String name){
-        SQLiteDatabase db = this.getWritableDatabase();
-        String query = "SELECT " + COL1 + " FROM " + TABLE_NAME +
-                " WHERE " + COL2 + " = '" + name + "'";
-        Cursor data = db.rawQuery(query, null);
-        return data;
-    }
-
-    /**
-     * Updates the name field
-     * @param newName
-     * @param id
-     * @param oldName
-     */
-    public void updateName(String newName, int id, String oldName){
-        SQLiteDatabase db = this.getWritableDatabase();
-        String query = "UPDATE " + TABLE_NAME + " SET " + COL2 +
-                " = '" + newName + "' WHERE " + COL1 + " = '" + id + "'" +
-                " AND " + COL2 + " = '" + oldName + "'";
-        Log.d(TAG, "updateName: query: " + query);
-        Log.d(TAG, "updateName: Setting name to " + newName);
-        db.execSQL(query);
-    }
-
-    /**
      * Delete from database
      * @param id
-     * @param name
      */
-    public void deleteName(int id, String name){
+    public void deleteName(int id){
         SQLiteDatabase db = this.getWritableDatabase();
         String query = "DELETE FROM " + TABLE_NAME + " WHERE "
-                + COL1 + " = '" + id + "'" +
-                " AND " + COL2 + " = '" + name + "'";
+                + "ID = '" + id + "'";
+
         Log.d(TAG, "deleteName: query: " + query);
-        Log.d(TAG, "deleteName: Deleting " + name + " from database.");
+        Log.d(TAG, "deleteName: Deleting from database.");
         db.execSQL(query);
     }
 
