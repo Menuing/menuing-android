ccefe23f2a6d75e3521a02228c01feeb7a9e9fdb
Alba Lamas <alba.v.lamas@gmail.com>
2019-01-06 17:24:57 +0100

    add dessert button

--- app/src/main/java/com/example/mrg20/menuing_android/activities/MealDetails.java
+++ app/src/main/java/com/example/mrg20/menuing_android/activities/MealDetails.java
@@ -318,8 +318,11 @@ public class MealDetails extends GlobalActivity implements View.OnClickListener 
                     case COCKTAIL:
                         url = new URL("http://" + ipserver + "/api/resources/recipes/getCocktail/?username=" + settings.getString("UserMail", ""));
                         break;
+                    case DESSERT:
+                        url = new URL("http://" + ipserver + "/api/resources/recipes/getCocktail/?username=" + settings.getString("UserMail", ""));
+                        break;
                     default:
-                        url = new URL("http://" + ipserver + "/api/resources/recipes/getRandom/?username=" + settings.getString("UserMail", ""));
+                        url = new URL("http://" + ipserver + "/api/resources/recipes/getDessert/?username=" + settings.getString("UserMail", ""));
                         break;
                 }
                 System.out.println(url);
